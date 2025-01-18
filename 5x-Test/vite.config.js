8203eabb066b87f345cb1eb83576b3add21a5bed
Tales <allen84324@gmail.com>
2025-01-19 02:07:19 +0800

    feature:新增環境變數

--- 5x-Test/vite.config.js
+++ 5x-Test/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
 	plugins: [vue()],
 	server: {
 		cors: true,
 		proxy: {
 			'/api': {
-				target: 'https://bank.5xcamp.us',
+				target: process.env.VITE_API_URL,
 				changeOrigin: true,
 				secure: false,
 				pathRewrite: (path) => path.replace(/^\/api/, ''),
